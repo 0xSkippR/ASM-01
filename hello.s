26c68c8169ba0e7302ad9d36fcf67e229371e326
0SkippR <amaury.de-bellabre@oteria.fr>
2026-02-01 22:10:11 +0100

    Test 2, overflow

--- hello.s
+++ hello.s
@@ -1,14 +1,12 @@
 global main
 main:
-    mov rax, 0x646c726f77206f
-    push rax
-    mov rax, 0x6c6c6568
+    mov rax, 0x646c726f77206f6c6c6568
     push rax
 
     mov rax, 1
     mov rdi, 1
     mov rsi, rsp
-    mov rdx, 11w
+    mov rdx, 16
     syscall
 
     mov rax, 60
